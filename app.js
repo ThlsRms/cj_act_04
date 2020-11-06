b68e80f4dfb95b509f532ec17ff525e60c0d59af
Thales Ramos <thlsrmsdev@gmail.com>
2020-11-06 14:22:43 +0100

    Trasnformando Notas-Ejercicio en modulos

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+const notas = require('./lib/notesHandler');
+
+/* 
+console.log('Crear nota', notas.crearNota('morales', 'Tampoco no caseros modesto moclearrales vapores ha el intimas.'));
+console.log('Borrar mala nota: ', notas.borrarNota('de VSCode'));
+console.log('Borrar nota: ', notas.borrarNota('truco de VSCode'));
+console.log('Ordenar por contenido', notas.ordernarNotas('cuerpo'));
+console.log('Ordenar por titulo', notas.ordernarNotas('titulo'));
+console.log('Buscar: epigonos', notas.buscarNotas('epigonos'));
+console.log('Buscar: yo', notas.buscarNotas('yo'));
+*/
